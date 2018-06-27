8363378e5a6980efe96e7ba13f236f810f921c70
Evelyn Magdalena Bejarano <evebejarano@hotmail.com>
2018-06-27 12:06:15 -0300

    Se agrego la capa de negocio con una unidad de trabajo y repositorios propuestos

---
+++ ColeKine/Modelos/UsuarioRoles/UserDetails.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ColeKine.Modelos
+{
+    public partial class UserDetails
+    {
+
+        public UserDetails()
+        {
+        }
+
+
+        [Key]
+        public int Id_UserDetails { get; set; }
+
+
+        public string UserName { get; set; }
+
+        public DateTime? LastModified { get; set; }
+        
+        public bool? Inactive { get; set; }
+
+        [StringLength(30)]
+        public string FirstName { get; set; }
+
+        [StringLength(30)]
+        public string LastName { get; set; }
+
+        public bool isSysAdmin { get; set; }
+
+
+        public virtual IdentityUser IdentityUsers { get; set; }
+
+
+    }
+}
---
+++ ColeKineBusinessLayer/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using ColeKine;
+using ColeKine.Modelos;
+using ColeKineBusinessLayer.Repositories;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ColeKineBusinessLayer.UnitOfWorks
+{
+    public class UnitOfWork : IDisposable                                                       
+    {
+        private ApplicationDbContext context = new ApplicationDbContext();
+        private RolesRepository<IdentityRole, RoleDetails> rolesRepository;
+        private UserRepository<IdentityUser, UserDetails> userRepository;
+        public GenericRepository<Permission> permissionRepository;
+
+        public RolesRepository<IdentityRole,RoleDetails> RolesRepository
+        {
+            get
+            {
+
+                if (this.rolesRepository == null)
+                {
+                    this.rolesRepository = new RolesRepository<IdentityRole,RoleDetails>(context);
+                }
+                return rolesRepository;
+            }
+        }
+
+        public UserRepository<IdentityUser,UserDetails> UserRepository
+        {
+            get
+            {
+
+                if (this.userRepository == null)
+                {
+                    this.userRepository = new UserRepository<IdentityUser,UserDetails>(context);
+                }
+                return userRepository;
+            }
+        }
+
+        public GenericRepository<Permission> PermissionRepository
+        {
+            get
+            {
+
+                if (this.permissionRepository == null)
+                {
+                    this.permissionRepository = new GenericRepository<Permission>(context);
+                }
+                return permissionRepository;
+            }
+        }
+
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
