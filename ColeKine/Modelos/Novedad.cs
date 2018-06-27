98ce8442325901db9b6a74587f028499a6151fe0
Maurixrs <mauriciooreyes@gmail.com>
2018-06-27 10:33:33 -0300

    revision tarjetas columna "Profesional" en featureMap

--- ColeKine/Modelos/Novedad.cs
+++ ColeKine/Modelos/Novedad.cs
@@ -7,7 +7,6 @@ namespace ColeKine.Modelos
     public abstract class Novedad
     {[Key]
         public int IdNovedad { get; set; }
-        public string Nombre { get; set; }
         public string Descripcion { get; set; }
         public DateTime FechaNovedad { get; set; }
         public DateTime FechaCarga { get; set; }
--- ColeKine/Modelos/Profesional/ProfesionalAportante.cs
+++ ColeKine/Modelos/Profesional/ProfesionalAportante.cs
@@ -11,6 +11,7 @@ namespace ColeKine.Modelos
         public virtual IList<ConvenioPrestadores> ConvenioPrestadoresFirmado { get; set; }
         public virtual IList<CuotaCajaPrevisional> CuotasAsociadas { get; set; }
 
+        public bool EsVoluntario { get; set; }// true si es voluntaria su inscripcion a la caja
 
         //public bool  Fallecido { get; set; }
         //public DateTime FechaFallecimiento{ get; set; }
